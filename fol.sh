cef6f7fbcf5587cb6d1b51ab8cfe2dd151e4e577
Rajatbairagi1323 <114255434+Rajatbairagi1323@users.noreply.github.com>
2023-08-07 13:06:09 +0530

    Create folder at a specific given path

--- fol.sh
+++ fol.sh
@@ -1,14 +1,22 @@
+#Script to create required number of directories/folders having date as folder name
+#Taking number of folder as input 
+#declare -i number
+#echo "Hello, How many folders you want to create?? : "
+#read number
+#Asking for path where folders has to be created
+#echo "Okay, tell me the path where you want to create folders?? (ex- ~/Desktop)"
+
 #! /bin/bash
 read -p "Hello, How many folders do you want to create? : " number
 read -p "path  where you want to create folders? : " path
 
 echo $path
 
-today=$(date +%y-%m-%d)
+today=$(date +%y-%m-%d)    # Date function
 
 # path existence 
 
-if [ -d "$path" ]
+if [ -d "$path" ]    # -d is for Directories
 then
   #echo "The path $path exists is a directory"
   for ((i=1; i<=number; i++))
