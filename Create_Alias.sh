183b20bdf8ee05cbeb85def21bb20cc3bc3deacc
Rajatbairagi1323 <114255434+Rajatbairagi1323@users.noreply.github.com>
2023-08-07 14:07:53 +0530

    Create Create_Alias.sh

---
+++ Create_Alias.sh
@@ -0,0 +1,11 @@
+#How to Create Alias Names?
+$ alias short_form='original command' # aliases we created are bydefault available only in the current session. 
+                                      # Once we close the terminal, all aliases will be lost.
+
+
+
+# We have to define our aliases in .bashrc file present in our home directory.
+$ alias short_form='original command'  
+
+# How to Use Unalias Command?
+unalias short_form='original command'
