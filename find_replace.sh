73302dbe5dffce4eb79e8e32b35da1f935113d1e
Rajatbairagi1323 <114255434+Rajatbairagi1323@users.noreply.github.com>
2023-08-07 12:40:48 +0530

    Update find_replace.sh

--- find_replace.sh
+++ find_replace.sh
@@ -1,36 +1,8 @@
+# Find and Replace programme to change the perticular word?
 #! /bin/bash
-
 read -p "Enter your file name: " file_name
 read -p "Enter your word you want to replace: " replace_word
 read -p "Enter your new word: " new_word
-
-
-sed -i 's/$replace_word/$new_word/g' $file_name
-echo "done"
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-sed -i 's/Andrew/James/g' /home/oleksandr/names.txt
+sed -i 's/$replace_word/$new_word/g' $file_name   # sed This is the command itself, indicating that you're using the sed utility
+echo "done"                                       #  -i This option tells sed to edit the file in-place
+                                                  #  s This indicates that you want to perform a substitution
